f279da0cdedf074e94abf30cd16aadd257e569b7
isabelle-zhan <zhanisabellle@gmail.com>
2025-03-26 09:25:25 +0100

    First architecture.

---
+++ Prototipe/distance.h
@@ -0,0 +1,12 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+#include "location.h"
+
+
+
+
+
+
+
+
+#endif /* DISTANCE_H */
---
+++ Prototipe/inout.h
@@ -0,0 +1,10 @@
+#ifndef INOUT_H
+#define INOUT_H
+
+#include "location.h"
+
+int fread_board(const char* file, Board board);
+
+
+
+#endif /* INOUT_H */
