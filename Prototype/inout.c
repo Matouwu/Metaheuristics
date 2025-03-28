2eb0f08b158f25d7ae884c433287c05db2c9c141
ElsPee <liyohan03@gmail.com>
2025-03-28 10:28:15 +0100

    read csv

--- Prototype/inout.c
+++ Prototype/inout.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "inout.h"
 
 int fread_board(const char* file, Board board){
-    FILE* f;
-    int i,j;
-    int entry;
+    FILE* f = fopen(file, "r");
+    int i = 0;
+    int j = 0;
+    char line[MAX_LINE_LENGTH];
 
-    f = fopen(file, "r");
     if (f == NULL){
         fprintf(stderr, "Erreur d'ouverture du fichier %s\n", file);
         return 0;
     }
 
+    fgets(line, MAX_LINE_LENGTH, f); /* Ignorer la première ligne */
 
+    while (fgets(line, MAX_LINE_LENGTH, f) != NULL) {
+        char* value = strtok(line, ",");
 
+        while (value != NULL) {
+            if (value != NULL) {
+                board[i][j++] = atoi(value);
+            }
+            value = strtok(NULL, ",");
+        }
 
+        i++;
+        j = 0;
+    }
+
+    fclose(f);
     return 1;
 }
