7f4d04f4d97700137cb59d7d0ae141afbea1630f
maxime <maxime.caillaud@edu.esiee.fr>
2025-03-28 09:30:23 +0100

    Calcul fitness + initialisation path

--- Prototype/genetic.c
+++ Prototype/genetic.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "genetic.h"
 
-void fitness(Board board){
+int fitness(Board board, Path path){
+    int fitness = 0;
+    int i;
+    for(i = 0; i < 84; i++){
+        fitness += board[path[i]][path[i+1]];
+    }
+    fitness += board[path[84]][path[0]];
+    return fitness;
+}
 
+void init_path(Path path){
+    int i, j;
+    for(i = 0; i < 85; i++){
+        path[i] = i;
+    }
 }
--- Prototype/genetic.h
+++ Prototype/genetic.h
@@ -1,4 +1,12 @@
 #ifndef GENETIC_H
 #define GENETIC_H
 
+#include "location.h"
+#include "inout.h"
+#include "graphic.h"
+
+int fitness(Board board, Path path);
+void randomize_path(Path path);
+void init_path(Path path);
+
 #endif /* GENETIC_H */
