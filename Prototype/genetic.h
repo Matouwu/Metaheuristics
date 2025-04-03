ca9cb5cc28f7cdec6f487e0485f657cd45468a03
maxime <maxime.caillaud@edu.esiee.fr>
2025-04-03 09:11:51 +0200

    Random path

--- Prototype/genetic.h
+++ Prototype/genetic.h
@@ -1,4 +1,12 @@
 #ifndef GENETIC_H
 #define GENETIC_H
 
+#include "location.h"
+#include "inout.h"
+#include "graphic.h"
+
+int calcul_fitness(Board board, Path path);
+void randomize_path(Board board, Path path);
+void init_path(Path path);
+
 #endif /* GENETIC_H */
--- Prototype/main.c
+++ Prototype/main.c
@@ -8,14 +8,23 @@
 #include "genetic.h"
 #include "graphic.h"
 
-
+  for (i = 0; i < 85; i++){
+    printf("%d ", path[i]);
+  }
 int main(int argc, char *argv[]){
 
   Board board;
   initBoard(board);
   fread_board(argv[1], board);
   display_board(board);
-
+  Path path;
+  init_path(path);
+  printf("%d\n",calcul_fitness(board, path));
+  int i;
+  for (i = 0; i < 15000; i++){
+    randomize_path(board, path);
+  }
+  printf("%d\n",calcul_fitness(board, path));
   return EXIT_SUCCESS;
 }
 
