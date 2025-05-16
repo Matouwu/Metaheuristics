ab44a751385d6b3914f3dcb5fc2327bc92c52e91
ElsPee <liyohan03@gmail.com>
2025-05-16 10:32:11 +0200

    new genetic

--- Prototype/genetic.h
+++ Prototype/genetic.h
@@ -5,11 +5,12 @@
 #include "inout.h"
 #include "graphic.h"
 
-#define POPULATION_SIZE 1000
-#define MAX_GENERATIONS 200
-#define TOURNAMENT_SIZE 50
-#define MUTATION_RATE 0.2
-#define CROSSOVER_RATE 0.8
+#define POPULATION_SIZE 500
+#define MAX_GENERATIONS 1000
+#define MUTATION_RATE 0.20
+#define CROSSOVER_RATE 0.85
+#define TOURNAMENT_SIZE 7
+#define STAGNATION_LIMIT 100
 
 typedef struct {
     Path path;
@@ -20,21 +21,26 @@ typedef struct {
     Individual members[POPULATION_SIZE];
     int generation;
     Individual best_ever;
+    int stagnation_count;
 } Population;
 
-int calcul_fitness(Board board, Path path);
-void randomize_path(Board board, Path path);
+int calculate_fitness(Board board, Path path);
 void init_path(Path path);
-
+void randomize_path(Board board, Path path);
 void init_population(Board board, Population *pop);
-void evolve_population(Board board, Population *pop);
+
 Individual tournament_selection(Population *pop);
+Individual find_best_individual(Population *pop);
+void evolve_population(Board board, Population *pop);
+
 void crossover(Individual *parent1, Individual *parent2, Individual *child1, Individual *child2);
 void mutate(Board board, Individual *individual);
 void copy_path(Path source, Path destination);
+
 void print_path(Path path);
-Individual find_best_individual(Population *pop);
 int path_contains(Path path, int city, int length);
-void get_best_path(Board board, Path best_path, int max_iterations);
+int is_valid_path(Path path);
+
+void solve_tsp(Board board, Path best_solution, int *best_fitness);
 
 #endif /* GENETIC_H */
