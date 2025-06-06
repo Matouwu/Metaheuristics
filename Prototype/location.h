1bbd36707460f03bf4aad2322bf5e6033cc3d740
ElsPee <liyohan03@gmail.com>
2025-06-07 01:39:53 +0200

    VRP first try

--- Prototype/location.h
+++ Prototype/location.h
@@ -1,12 +1,27 @@
-#ifndef POSITION_H
-#define POSITION_H
+#ifndef LOCATION_H
+#define LOCATION_H
 
-typedef int Board[85][85];
-typedef int Path[85];
+#define NUM_CITIES 85
+#define MAX_VEHICLES 20
+#define MAX_ROUTE_LENGTH 50
+#define MAX_TIME 10800 // 3 heures en secondes (3*3600)
+#define DEPOT 0
+
+typedef int Board[NUM_CITIES][NUM_CITIES];
+
+typedef struct {
+    int path[MAX_ROUTE_LENGTH];
+    int length;
+    int duration;
+} Route;
+
+typedef struct {
+    Route routes[MAX_VEHICLES];
+    int num_vehicles;
+    int total_duration;
+} Solution;
 
 void initBoard(Board board);
 void display_board(Board board);
 
-
-
-#endif /* POSITION_H */
+#endif /* LOCATION_H */
